ea1d74e1fd98fb921a988dadc482945fd8c50f3a
sooraj <soorajparkkattil@gmail.com>
2017-11-19 20:23:57 +0530

    linking

--- django/doctors/models.py
+++ django/doctors/models.py
@@ -4,11 +4,23 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 # Create your models here.
-class Doctor(models.Model):
+class Department(models.Model):
     name = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    count=models.IntegerField(default=0)
 
     def __str__(self):
+        return 'Department #{0} - {1}'.format(self.pk, self.name)
+    def countit(self):
+        self.count=self.count+1
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=150)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, default=None, null=True)
+    qualifiaction=models.CharField(max_length=200,null=True)
+    def __str__(self):
         return 'Doctor #{0} - {1}'.format(self.pk, self.name)
+
 
 class Appointment(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,default=None,null=True)
