bad4cecceb01196a22c987e8690fdedabf9905c0
sooraj <soorajparkkattil@gmail.com>
2017-11-20 00:02:21 +0530

    finalized and sorted into a folder

--- django/doctors/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Doctor,Appointment,Department
-
-# Register your models here.
-admin.site.register(Doctor)
-admin.site.register(Appointment)
-admin.site.register(Department)
